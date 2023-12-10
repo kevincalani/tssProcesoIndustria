cc26a99307c80327da3e65b6eddf56e68e5bd820
Kevin Calani <kevincalani2311@gmail.com>
2023-12-10 15:53:17 -0600

    crenado conclusiones con parametros configurables

--- front/screens/Conclusiones.js
+++ front/screens/Conclusiones.js
@@ -1,15 +1,90 @@
-import React from 'react'
-import { SafeAreaView, View, Text, StyleSheet, Image, Button, ScrollView } from 'react-native'
+import React, {useState, useEffect}from 'react'
+import { SafeAreaView, View, Text, StyleSheet, ScrollView, TextInput } from 'react-native'
+import jstat from 'jstat';
 
-export default function Conclusiones() {
+export default function Conclusiones({route}) {
+  const tableData = route?.params?.tableData || [];
+  const distribucion = route?.params?.distribucion || "";
+  const [nivelSeguridad, setNivelSeguridad] = useState(95);
+  const [valorDif, setValorDif] = useState(0.01);
+  const [cantDatos, setCantDatos]= useState(tableData.length);
+
+  const buscarZ = (area) => {
+    try {
+      const z = (jstat.normal.inv(area, 0, 1)*-1);
+
+      return z.toFixed(2);
+    } catch (error) {
+      console.error('Error al buscar en la tabla de distribución normal:', error);
+      return null;
+    }
+  };
+
+  const porcentajeInterferencia =(tableData) =>{
+    if (tableData.length === 0) {
+      return 0; // Evitar división por cero si no hay datos
+    }
+  
+    let contador = 0;
+  
+    for (const item of tableData) {
+      if (item.interseccion === 1) {
+        contador++;
+      }
+    }
+  
+    return (contador / tableData.length) * 100;
+  };
+
+  const [porcentaje, setPorcentaje]= useState(() => porcentajeInterferencia(tableData));
+
+  useEffect(() => {
+    setCantDatos(tableData.length);
+    setPorcentaje(porcentajeInterferencia(tableData));
+  }, [tableData]);
+
+  const renderCont = () => {
+    if (tableData.length === 0) {
+      return <Text style={styles.noDataText}>Para mostrar el informe, genere el informe con la tabla de datos desde la pestaña de Resultados.</Text>;
+    }
+
+    else {
+      const alpha = (100 - nivelSeguridad) / 100; // Calcula alpha basado en el nivel de seguridad
+      const error = (alpha.toFixed(2)*(Math.sqrt(porcentaje*(100-porcentaje))/valorDif));// Calcula Estimacion del error
+      const cantSimu =(error*error);//calcula la cantidad de simualciones requeridas
+      return (
+        <View>
+        <Text style={styles.subtitle}>a) Utilizando la {distribucion} de un total de {cantDatos} simulaciones se encontro que a probabilidad de que haya interferencia es de {porcentaje}%:</Text>
+
+      <Text style={styles.subtitle}>b) Tomando en cuenta el resultado del inciso A, tenemos que el porcentaje de interferencia es de {porcentaje}</Text>
+      <Text style={styles.subtitle}> Además, considerando que {(100 - porcentaje)} es el porcentaje restante y si el nivel de seguridad es del{' '}
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              value={nivelSeguridad}
+              onChangeText={(text) => setNivelSeguridad(text)}
+            />%
+             entonces α = {alpha.toFixed(2)} por lo tanto Zα/2= {alpha.toFixed(2)/2}</Text> 
+             <Text style={styles.subtitle}>Buscando en la tabla  de distribucion normal Zα/2= {buscarZ(alpha.toFixed(2)/2)}</Text> 
+             <Text style={styles.subtitle}>Y teniendo el valor minimo  de diferencia es de:{' '}
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              value={valorDif}
+              onChangeText={(text) => setValorDif(text)}
+            /></Text> 
+      <Text style={styles.subtitle}>Usando la formula de la estimacion del error se tiene: {error.toFixed(5)}</Text> 
+      <Text style={styles.subtitle}>Por lo tanto el numero de simualciones a realizar si se quiere que la probabilidad de interferencia estimada difiera de su valor verdadero en menos de {valorDif}, con un nivel de seguridad del {nivelSeguridad}% es el de: {cantSimu.toFixed(1)} veces</Text></View>
+      );
+    }
+  };
+
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <View style={styles.container}>
       <Text style={styles.title}>Informe de Simulación</Text>
            
-      <Text style={styles.subtitle}>a) La probabilidad de que haya interferencia es de :</Text>
-
-      <Text style={styles.subtitle}>b) El número de veces que es necesario simular el experimento si se quiere que la probabilidad de interferencia estimada difiera de su valor verdadero en menos de 0.01, con un nivel de seguridad del 95% es el de:</Text>
+      {renderCont()}
 
     </View>
     </ScrollView>
@@ -36,6 +111,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 8,
     textAlign: 'center',
+    paddingBottom: 24,
   },
   subtitle: {
     fontSize: 16,
@@ -52,5 +128,20 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  noDataText: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 16,
+    color: 'gray',
+  },
+  input: {
+    height: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 30,
+    textAlign: 'center',
+    marginLeft: 5,
+    marginRight: 5,
   },
 });
--- front/screens/TablaResul.js
+++ front/screens/TablaResul.js
@@ -1,38 +1,66 @@
 import React, {useState} from 'react'
-import { ScrollView, View, FlatList, Text, StyleSheet } from 'react-native'
+import { ScrollView, View, FlatList, Text, StyleSheet,Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
 
-export default function TablaResul({ navigation, route }) {
+export default function TablaResul({ route }) {
   const tableData = route?.params?.tableData || [];
+  const distribucion = route?.params?.distribucion || "";
+  const navigation = useNavigation();
+
+  const renderCont = () => {
+    if (tableData.length === 0) {
+      return <Text style={styles.noDataText}>Para mostrar los datos, primero elija una distribución y complete los datos.</Text>;
+    }
+
+    else {
+      return (
+        <View style={styles.button}>
+        <Button title="Generar Informe De Simulacion" color="#0F52BA" onPress={navegacionPantallas}/>
+        </View>
+        
+      );
+    }
+  };
+ 
+  const navegacionPantallas = async () => {
+    try {
+      navigation.navigate('Conclusiones', { tableData: tableData, distribucion:distribucion });
+    } catch (error) {
+      console.error("Error al generar datos:", error);
+    }
+  };
 
   return (
     <ScrollView>
-        <View style={styles.container}>
-      <FlatList
-        data={tableData}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.row}>
-            <View style={styles.idContainer}>
-              <Text style={styles.idText}>{item.id}</Text>
+      <View style={styles.container}>
+        <FlatList
+          data={tableData}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.row}>
+              <View style={styles.idContainer}>
+                <Text style={styles.idText}>{item.id}</Text>
+              </View>
+              <Text style={styles.cell}>{item.x1}</Text>
+              <Text style={styles.cell}>{item.x2}</Text>
+              <Text style={styles.cell}>{item.interseccion}</Text>
             </View>
-            <Text style={styles.cell}>{item.x1}</Text>
-            <Text style={styles.cell}>{item.x2}</Text>
-            <Text style={styles.cell}>{item.interseccion}</Text>
-          </View>
-        )}
-        ListHeaderComponent={() => (
-          <View style={styles.header}>
-            <Text style={styles.headerText}>N°</Text>
-            <Text style={styles.headerText}>Valor de X1</Text>
-            <Text style={styles.headerText}>Valor de X2</Text>
-            <Text style={styles.headerText}>Interseccion</Text>
-          </View>
-        )}
-      />
-    </View>
+          )}
+          ListHeaderComponent={() => (
+            <View style={styles.header}>
+              <Text style={styles.headerText}>N°</Text>
+              <Text style={styles.headerText}>Valor de X1</Text>
+              <Text style={styles.headerText}>Valor de X2</Text>
+              <Text style={styles.headerText}>Interseccion</Text>
+            </View>
+          )}
+          ListFooterComponent={renderCont}
+        />
+      </View>
     </ScrollView>
-  )
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -41,13 +69,13 @@ const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center', 
+    alignItems: 'center',
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
     paddingVertical: 8,
   },
   idContainer: {
-    width: 30, 
+    width: 30,
   },
   idText: {
     textAlign: 'center',
@@ -68,5 +96,22 @@ const styles = StyleSheet.create({
   headerText: {
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  noDataText: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 16,
+    color: 'gray',
+  },
+  button: {
+    paddingTop: 16, 
+    alignItems: "center",
+    justifyContent: "center",
+    rowGap: 30,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
